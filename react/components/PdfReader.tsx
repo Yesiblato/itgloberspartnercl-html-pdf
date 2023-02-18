f5f639df23bb31cfa9b0ff6d2616ecce857c67e3
Yesiblato <87024446+Yesiblato@users.noreply.github.com>
2023-02-17 23:48:03 -0500

    feat: created and styles of pdf component

---
+++ react/components/PdfReader.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+
+type Props = {
+  pdfUrl: string,
+  width: number,
+  height: number
+}
+
+const PdfReader = ({pdfUrl, width, height}: Props) => {
+  const [mounted, SetMounted] = useState(false)
+
+  useEffect(() => {
+    SetMounted(true)
+  }, [])
+
+  return (
+    mounted && (
+      <div className='flex justify-center'>
+        <object
+        data = {pdfUrl}
+        type = "apllication/pdf"
+        width = {width}
+        height = {height}
+        >
+          <iframe title='PDF'src={pdfUrl} width={width} height={height}>
+            <p>!Este navegador no soporta PDF!</p>
+          </iframe>
+
+        </object>
+      </div>
+    )
+  )
+}
+
+export default PdfReader
+
